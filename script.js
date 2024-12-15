6b47c99fc61e95753895ac359e17953b1e18de61
King <kingsley.adarkwah@gmail.com>
2024-12-15 18:32:24 -0500

    add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// Create the map
+var map = L.map('map').setView([ /* Your Latitude */, /* Your Longitude */ ], /* Your Zoom Level */ );
+
+// Add a basemap tile layer (you can choose a different one if you like)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+// Load your GeoJSON layers (replace with your actual file names)
+var layer1 = L.geoJSON(null); // Initialize empty layers
+var layer2 = L.geoJSON(null);
+
+fetch('layer1.geojson')
+    .then(response => response.json())
+    .then(data => {
+         layer1.addData(data);
+    });
+
+fetch('layer2.geojson')
+    .then(response => response.json())
+    .then(data => {
+        layer2.addData(data);
+    });
+
+
+
+
+// Create control for layer visibility
+var overlayMaps = {
+    "Layer 1": layer1,
+    "Layer 2": layer2
+};
+
+L.control.layers(null, overlayMaps).addTo(map);
