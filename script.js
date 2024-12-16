2c6b4eff3a50ee2d6ec9d7d95f956a82747325e6
King <kingsley.adarkwah@gmail.com>
2024-12-15 19:02:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create the map
-var map = L.map('map').setView([71.505, -3.09], 13));
+var map = L.map('map').setView([71.505, -3.09], 13);
 
 // Add a basemap tile layer (you can choose a different one if you like)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
